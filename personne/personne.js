53316b235c1565ca59fd8fd543141b5aff76b53d
aimielbn <144000676+aimielbn@users.noreply.github.com>
2023-09-26 09:08:18 +0200

    articles

--- personne/personne.js
+++ personne/personne.js
@@ -18,16 +18,9 @@ const detailsPersonne = () => {
 
   return `
       <div class="card col col-sm-10 col-md-8 col-xl-6 mx-auto">
-        <img src="${article.avatar}" class="card-img-top" alt="avatar de ${article.prenom} ${article.nom}">
+        <img src="${article.avatar}" class="card-img-top" alt="avatar de ${article.titre} ${article.contenu}">
         <div class="card-body">
-          <h5 class="card-title">${article.prenom} ${article.nom}</h5>
-          <p class="card-text">
-         <h6> ${article.date_de_naissance}</h6>
-         <a href="tel:+32499289591"> ${article.numero_de_telephone}</a>
-         <a href="mailto:aimie.lebon@hotmail.com"/> ${article.adresse_email}</a><br>
-          ${article.description}
-            
-          </p>
+          <h5 class="card-title">${article.titre} ${article.contenu}</h5>
         </div>
       </div>
     `;
