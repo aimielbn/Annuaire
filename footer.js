5156e472dc8d1d919b52a8e1eaa1a83cec80f851
aimielbn <144000676+aimielbn@users.noreply.github.com>
2023-09-26 11:42:00 +0200

    2 files modified, 1 added

---
+++ footer.js
@@ -0,0 +1,17 @@
+// footer.js
+
+// Données JSON pour le footer
+const footerData = {
+  nomSite: "Miraculous",
+  annee: 2023,
+  droits: "Tous droits réservés"
+};
+
+// Fonction pour afficher les données JSON dans le footer
+function afficherFooterData(data) {
+  const footerContent = document.getElementById("footerContent");
+  footerContent.textContent = `&copy; ${data.annee} ${data.nomSite}. ${data.droits}`;
+}
+
+// Appeler la fonction pour afficher les données dans le footer
+afficherFooterData(footerData);
